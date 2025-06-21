b514b1bb95ecbb3d230348be93622e9be080d9c2
Farhod <farhod@example.com>
2025-06-21 21:25:47 +0500

    admin.py

--- task/admin.py
+++ task/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from task.models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('title', 'content', 'author__username')
+    ordering = ('-created_at',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'author',  'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('text', 'author__username', 'post__title')
+    ordering = ('-created_at',)
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from django.contrib.auth.models import User
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(DefaultUserAdmin):
+    list_display = ('id', 'username', 'email', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email')
 
